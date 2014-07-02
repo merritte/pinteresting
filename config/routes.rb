160229b99e3a069c51561a91aee0225f3ebb943d
Shay-Jahen Merritte <Shay@Merritte.com>
2014-07-02 19:26:28 -0400

    Removed the contact page

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Pinteresting::Application.routes.draw do
   devise_for :users
   root "pins#index"
   get "about" => "pages#about" # creates about_path
-  get "contact" => "pages#contact"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
